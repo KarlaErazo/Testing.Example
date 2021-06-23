cc941d7f25f0fe5dacdce7492163d019378367af
PoolMart <51102992+PoolMart@users.noreply.github.com>
2021-06-23 13:04:58 -0500

    Agregar archivos de proyecto.

---
+++ Testing.Example/FactorialService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testing.Example
+{
+   public class FactorialService
+    {
+        public int Calculate(int number)
+        {
+
+            if (number == 0) {
+                return 1;
+
+            }
+            int result = 1;
+            for (int index = 1; index <= number; index++) {
+                result *= index;
+            }
+
+            return result;
+        }
+
+        
+    }
+}
